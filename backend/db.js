8cf261b93a00766b9f218714f76086c63987ad97
shalev ofir <50710665+ofirent@users.noreply.github.com>
2025-04-12 19:44:30 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -6,6 +6,9 @@ const pool = new Pool({
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
   port: process.env.PGPORT,
+  ssl: {
+    rejectUnauthorized: false, // מאפשר חיבור עם SSL גם אם לא מאמתים את השרת
+  },
 });
 
 pool.connect()
